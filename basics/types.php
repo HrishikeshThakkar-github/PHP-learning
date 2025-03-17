4f1e332a1692d5eeb3fbabeecd17b573ad39c865
HrishikeshThakkar-github <hrishi.pvt@gmail.com>
2025-03-17 14:05:36 +0530

    TE-T349 PR-57: gone through string and string functions, numbers and casting

--- basics/types.php
+++ basics/types.php
@@ -111,5 +111,79 @@ $$a = "world"; // Now a variable named $hello is created with value "world"
 echo "$a {$$a}", "\n"; // Output: hello world
 echo "$a $hello", "\n"; // Output: hello world (same as above)
 
+//Numbers
+echo PHP_INT_MAX;
+echo PHP_INT_MAX;
+echo PHP_INT_SIZE;
+ECHO "\n";
+ECHO "\n";
+var_dump( is_int($a));
 
+
+
+$foo = 1 + "10.5";  
+echo "\n",$foo;
+var_dump($foo);             // $foo is float (11.5)
+$foo = 1 + "-1.3e3";              // $foo is float (-1299)
+echo "\n",$foo;
+var_dump($foo);
+$foo = 1 + "10 Small Pigs";       // $foo is integer (11) and an E_WARNING is raised in PHP 8.0.0, E_NOTICE previously
+echo "\n",$foo;
+var_dump($foo);
+$foo = 4 + "10.2 Little Piggies"; // $foo is float (14.2) and an E_WARNING is raised in PHP 8.0.0, E_NOTICE previously
+echo "\n",$foo;
+var_dump($foo);
+$foo = "10.0 pigs " + 1;          // $foo is float (11) and an E_WARNING is raised in PHP 8.0.0, E_NOTICE previously
+echo "\n",$foo;
+var_dump($foo);
+$foo = "10.0 pigs " + "10.0 pigs ";
+echo "\n",$foo;
+var_dump($foo);        // $foo is float (20) 
+
+echo "\n";
+
+//casting
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean  for boolean the output is "1" or "0"
+$e = NULL;    // NULL  for null the output we get is empty string
+
+$a = (string) $a;
+$b = (string) $b;
+$c = (string) $c;
+$d = (string) $d;
+$e = (string) $e;
+
+//To verify the type of any object in PHP, use the var_dump() function:
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "25 kilometers"; // String
+$d = "kilometers 25"; // String  
+$e = "hello"; // String
+$f = true;    // Boolean
+$g = NULL;    // NULL
+
+$a = (int) $a;
+$b = (int) $b;
+$c = (int) $c;
+$d = (int) $d;  // this will be 0
+$e = (int) $e;  //  this will be 0
+$f = (int) $f;
+$g = (int) $g;
+
+$y = 1 + (int)"kabir 10.1" ;
+var_dump($y);  
+$x = 1 + (int)"10.1 kabir" ;
+var_dump($x);  
+$z = 1 + (int)"10.1 ka 10.1 bir" ;
+var_dump($z);  
+echo $y," ",$x," ",$z
 ?>
